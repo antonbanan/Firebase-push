46170d36e0ad8b36d587276ec0b0ed3bd7995c83
anton <antbongen@gmail.com>
2019-12-02 09:53:34 +0200

    return

--- public/firebase_subscribe.js
+++ public/firebase_subscribe.js
@@ -5,72 +5,72 @@ firebase.initializeApp({
 
 // браузер поддерживает уведомления
 // вообще, эту проверку должна делать библиотека Firebase, но она этого не делает
-if('Notification' in window){
+if ('Notification' in window) {
 	var messaging = firebase.messaging();
 
 	// пользователь уже разрешил получение уведомлений
 	// подписываем на уведомления если ещё не подписали
-	if(Notification.permission === 'granted'){
+	if (Notification.permission === 'granted') {
 		subscribe();
 	}
 
 	// по клику, запрашиваем у пользователя разрешение на уведомления
 	// и подписываем его
-	$('#subscribe').on('click', function(){
+	$('#subscribe').on('click', function () {
 		subscribe();
 	});
 }
 
-function subscribe(){
+function subscribe() {
 	// запрашиваем разрешение на получение уведомлений
 	messaging.requestPermission()
-		.then(function (){
+		.then(function () {
 			// получаем ID устройства
 			messaging.getToken()
-				.then(function (currentToken){
+				.then(function (currentToken) {
 					console.log(currentToken);
 					$('#tokken').text(currentToken);
 
-					if(currentToken){
+					if (currentToken) {
 						sendTokenToServer(currentToken);
-					}else{
+					} else {
 						console.warn('Не удалось получить токен.');
 						setTokenSentToServer(false);
 					}
 				})
-				.catch(function(err){
+				.catch(function (err) {
 					console.warn('При получении токена произошла ошибка.', err);
 					setTokenSentToServer(false);
 				});
 	})
-	.catch(function(err){
+	.catch(function (err) {
 		console.warn('Не удалось получить разрешение на показ уведомлений.', err);
 	});
 }
 
 // отправка ID на сервер
-function sendTokenToServer(currentToken){
-	if(!isTokenSentToServer(currentToken)){
+function sendTokenToServer(currentToken) {
+	if (!isTokenSentToServer(currentToken)) {
 		console.log('Отправка токена на сервер...');
 
 		var url = ''; // адрес скрипта на сервере который сохраняет ID устройства
-		$.post(url,{
+		$.post(url, {
 			token: currentToken
 		});
 
 		setTokenSentToServer(currentToken);
-	}else{
+	} else {
 		console.log('Токен уже отправлен на сервер.');
 	}
 }
 
 // используем localStorage для отметки того,
 // что пользователь уже подписался на уведомления
-function isTokenSentToServer(currentToken){
+function isTokenSentToServer(currentToken) {
 	return window.localStorage.getItem('sentFirebaseMessagingToken') == currentToken;
 }
 
-function setTokenSentToServer(currentToken){
+function setTokenSentToServer(currentToken) {
 	window.localStorage.setItem(
 		'sentFirebaseMessagingToken',
 		currentToken ? currentToken : ''
