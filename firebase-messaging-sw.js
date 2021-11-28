90a8f2d6f1a4b6594f11958a05c75101c7e3e514
Anton Bondarevskiy <antbongen@gmail.com>
2021-11-28 19:55:13 +0200

    sss

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -7,21 +7,21 @@
 // 		});
 // 	}
 
-importScripts('https://www.gstatic.com/firebasejs/8.10/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/8.10/firebase-messaging.js');
+// importScripts('https://www.gstatic.com/firebasejs/8.10/firebase-app.js');
+// importScripts('https://www.gstatic.com/firebasejs/8.10/firebase-messaging.js');
 
-var firebaseConfig = {
-	apiKey: "AIzaSyBjzSpn8TJOlLm5kspJ1KDgFYslUS-ikXQ",
-	authDomain: "shelf-6eaeb.firebaseapp.com",
-	projectId: "shelf-6eaeb",
-	storageBucket: "shelf-6eaeb.appspot.com",
-	messagingSenderId: "728443310744",
-	appId: "1:728443310744:web:79726a26292c5aa178b6ed",
-	measurementId: "G-2YVW7854YF"
-};
+// var firebaseConfig = {
+// 	apiKey: "AIzaSyBjzSpn8TJOlLm5kspJ1KDgFYslUS-ikXQ",
+// 	authDomain: "shelf-6eaeb.firebaseapp.com",
+// 	projectId: "shelf-6eaeb",
+// 	storageBucket: "shelf-6eaeb.appspot.com",
+// 	messagingSenderId: "728443310744",
+// 	appId: "1:728443310744:web:79726a26292c5aa178b6ed",
+// 	measurementId: "G-2YVW7854YF"
+// };
 
 
-firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
 
-console.log('sho')
-const messaging = firebase.messaging();
+// console.log('sho')
+// const messaging = firebase.messaging();
